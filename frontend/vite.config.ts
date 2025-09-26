6c52c708811c3236fd4a5282dda056e8e0d2120d
Tyler Pak <tyler.j.pak.mil@army.mil>
2025-09-26 11:35:32 -0500

    checkpoint reached

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,17 @@
+/// <references types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+    build: {
+        outDir: "build",
+    },
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './src/setupTests.ts',
+        css: true,
+    }
+});
